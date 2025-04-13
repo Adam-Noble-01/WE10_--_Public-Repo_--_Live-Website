4ce04cbd6b07f31488e38b611eb0f269cfe42fb7
Adam-Noble-01 <Adam@noble-architecture.com>
2025-04-13 14:48:25 +0100

    minor

--- na-apps/NA20_WebApp_-_PlanVision - Copy/00_-_Dev-Tools/query-json-file-metadata-section.js
+++
@@ -1,8 +0,0 @@
-fetch("https://www.noble-architecture.com/assets/AD01_-_DATA_-_Common_-_Global-Data-Library/AD01_10_-_DATA_-_Common_-_Core-Web-Asset-Library.json")
-    .then(r => r.json())
-    .then(data => {
-        const meta = data["File_Metadata"];
-        for (const key in meta) {
-            console.log(`KEY = ${key}  →  VALUE = "${meta[key]}"`); 
-        }
-    });
